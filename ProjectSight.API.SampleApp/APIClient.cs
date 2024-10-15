6db5cf4c8593830941fdb96d9f92784a9289293f
Karl Costenbader <KCosten@am.trimblecorp.net>
2024-10-15 14:05:16 -0700

    Fixed default configuration for API-X sample app

--- ProjectSight.API.SampleApp/APIClient.cs
+++ ProjectSight.API.SampleApp/APIClient.cs
@@ -31,7 +31,7 @@ namespace ProjectSight.API.SampleApp
         public const string PROJECTSIGHT_AZURE_ENDPOINT_TEST = "https://api-stg-ap1.trimblepaas.com/projectsightops1-v1.0";
 
         public const string TID_ENDPOINT = TID_ENDPOINT_PROD;
-        public const string PROJECTSIGHT_ENDPOINT = PROJECTSIGHT_ENDPOINT_STAGING;
+        public const string PROJECTSIGHT_ENDPOINT = PROJECTSIGHT_ENDPOINT_PROD;
 
         // *** this will be called by TID`s redirect ***
         // care should be taken that the given URL will not cause unwanted behavior on the test system
